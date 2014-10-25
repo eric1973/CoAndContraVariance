bd7f5694ed3f87ee5182d953ab6a8f632069d6db
erich brunner <erich.brunner@live.at>
2014-10-25 04:20:36 +0200

    covariance sample

--- CoAndContraVariance/Program.cs
+++ CoAndContraVariance/Program.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace CoAndContraVariance
 {
-    public class Base { public string Name { get; set; } }
+    public class Base { 
+        public string Name { get; set; }
+        public override string ToString()
+        {
+            return "greetings from Base class: " + this.Name;
+        }
+    }
     public class Derived : Base
     {
         public Derived()
@@ -17,48 +23,68 @@ namespace CoAndContraVariance
         {
             this.Name = name;
         }
+
+        public override string ToString()
+        {
+            return "greetings from Derived class: " + this.Name;
+        }
     }
 
     class Program
     {
-        public static Derived MyMethod(Base b, string name)
+        public delegate TResult MyFunc<T1, T2, /*out*/ TResult>(T1 t1, T2 t2);
+
+        public static Derived F2(Base b, string name)
         {
             b.Name = name;
             return b as Derived ?? new Derived(name);
         }
-        
+
+
+        public static void UseMyFunc(MyFunc<Base, string, Base> func, Base printme, string message)
+        {
+            Console.WriteLine(func(printme, message).ToString());
+        }
+        interface IMyInterface<out T>
+        {
+            T Value { get; }
+        }
+
+        class Genericclass<T> : IMyInterface<T>
+        {
+
+            public T Value {get; private set; }
+
+            public Genericclass(T value)
+            {
+                this.Value = value;
+            }
+        }
         static void Main(string[] args)
         {
             /*
-             * Func<in T, out TResult> f1
-             * in T = Base
-             * out TResult = Derived
-             * */
-            Func<Base, string, Derived> f1 = MyMethod;
-
-            // Covariant return type.
-
-            /*
-             * Func<in T, out TResult> f2
-             * in T = Base
-             * out TResult = Base => f1's TResult (Derived) 
-             * is more specific than f2's TResult (Base). 
+             * Notice the formal func parameter's TResult which is Base!
+             * You can pass in methods which return a Base or Derived instance
              * 
-             * */
-            Func<Base, string, Base> f2 = f1; 
-            Base b2 = f2(new Derived(), "derived");
+             * Advantage: a more general purpose use of method UseFunc
+             */
+            MyFunc<Base, string, Derived> fp1 = F2;
+            MyFunc<Base, string, Base> fp2 = F2;
 
-            // Contravariant parameter type.
 
-            /*
-             * Func<in T, out TResult> f3
-             * in T = Derived => f3's T (Derived) is more specific
-             * than f1's T (Base)
-             * out TResult = Derived 
-             * 
-             * */
-            Func<Derived, string, Derived> f3 = f1;
-            Derived d3 = f3(new Derived(), "derived");
+            MyFunc<Base, string, Derived> fp3 = F2;
+            
+            //MyFunc<Base, string, Base> fp4 = fp3; No way. Uncomment the out in MyFunc and that line compiles too.
+
+            UseMyFunc(F2, new Base(), " base message");
+            UseMyFunc(F2, new Derived(), " derived message");
+
+            var derived = new Genericclass<Derived>(new Derived("derived"));
+            IMyInterface<Derived> iDerived = derived;
+
+            IMyInterface<Base> iBase = derived;
+
+            Console.ReadKey();
         }
     }
 }
